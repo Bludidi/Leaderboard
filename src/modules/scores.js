8f74f5deab46718f122ba202d73f671f46dfc50d
Bludidi <bludidi5@gmail.com>
2022-10-25 22:52:59 +0200

    fix lint errors

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,23 +1,3 @@
-// const scores = [
-//   { Name: 'Name', score: 100 },
-//   { Name: 'Name', score: 20 },
-//   { Name: 'Name', score: 50 },
-//   { Name: 'Name', score: 78 },
-//   { Name: 'Name', score: 125 },
-//   { Name: 'Name', score: 77 },
-//   { Name: 'Name', score: 42 },
-// ];
-// const scoresData = document.getElementById('table');
-// let playerData = '';
-
-// scores.forEach((player) => {
-//   playerData += `
-//   <li>${player.Name} : ${player.score}</li>
-// `;
-// });
-
-// scoresData.innerHTML = playerData;
-
 const form = document.querySelector('form');
 const refreshBtn = document.getElementById('refresh');
 const scoreTable = document.getElementById('table');
